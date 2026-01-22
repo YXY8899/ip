f3b6b5d2608f8fde5389dfa6833d81de6379547b
YXY8899 <yeoxinyu9@gmail.com>
2026-01-22 20:36:41 +0800

    Complete Level-1

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Jarvis {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
-                + "Hello! I'm Jarvis\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
-        System.out.println(logo);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Jarvis");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        String input = "";
+        while (!input.equals("bye")) {
+            input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+
+        scanner.close();
     }
 }
